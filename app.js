b6e88531767ba5bf7d62222405e0f1e94983a02a
martin jerez <martincho111986@gmail.com>
2020-04-26 01:52:19 -0300

    cambiando name index por app.js y agregando require

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const router = express.Router();
 
 mongoose.connect('mongodb://localhost:27017/biblioteca', {useNewUrlParser: true, useUnifiedTopology: true});
